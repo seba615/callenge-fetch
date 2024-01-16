7820b591988a88f4ddf78191a90a5e767906fcba
Seba <sebase.pereyra@gmail.com>
2024-01-16 19:56:33 -0300

    Add function to handle click

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { getRandomFact } from "./services/facts";
 import './App.css';
 
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red&json=true`;
@@ -10,31 +11,31 @@ export function App() {
 
     const [fact, setFact] = useState();
     const [image, setImage] = useState('');
-
-    useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDON_FACT_URL)
-            .then(response => response.json())
-            .then(factData => {
-                const newFact = factData.fact;
-                setFact(newFact)
-            })
-    }, [])
     
     useEffect(() => {
-        if(!fact)
+        getRandomFact().then(newFact => setFact(newFact));
+    }, [])
+
+    useEffect(() => {
+        if (!fact)
             return;
-        const firstWord = fact.split(' ', 1).join(' ');        
+        const firstWord = fact.split(' ', 1).join(' ');
         fetch('https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true')
-        .then(response => response.json())
-        .then(imgData => {
-            const { _id } = imgData;
-            setImage({ id: _id, word: firstWord });
-        })
+            .then(response => response.json())
+            .then(imgData => {
+                const { _id } = imgData;
+                setImage({ id: _id, word: firstWord });
+            })
     }, [fact])
+
+    const handleUpdate = () => {
+        getRandomFact().then(newFact => setFact(newFact));
+    }
 
     return (
         <main>
             <h1>Fetching con gatos</h1>
+            <button onClick={handleUpdate}> Refresh </button>
             {fact && <p>{fact}</p>}
             {image && <img src={`https://cataas.com/cat/${image.id}/says/${image.word}`} alt="Image extracted using Cataas API" />}
         </main>
---
+++ src/services/facts.js
@@ -0,0 +1,10 @@
+const CAT_ENDPOINT_RANDON_FACT_URL = `https://catfact.ninja/fact`;
+
+export const getRandomFact = () => {
+    return fetch(CAT_ENDPOINT_RANDON_FACT_URL)
+        .then(response => response.json())
+        .then(factData => {
+            const newFact = factData.fact;
+            return newFact
+        })
+}
